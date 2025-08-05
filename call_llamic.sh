801c5c83cf3717346658f755400a4601e78b4fa8
diogomataloto <147916264+miguelm1970@users.noreply.github.com>
2025-08-05 16:15:49 +0100

    Create call_llamic.sh

---
+++ call_llamic.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=MIMIC_NER_example
+#SBATCH --output=MIMIC_NER_example.out
+#SBATCH --gres=gpu
+#SBATCH --partition=compute
+
+task=MIMIC_example
+huggingface-cli login --token YOUR_TOKEN
+MODEL_PATH_NER=models/llama_ner-4290
+MODEL_PATH_NEL=models/llama_nel-1500
+MODEL_PATH_REVIEW=llama3.1
+
+datadir=data
+outdir=runs/$task
+mkdir -p $outdir
+
+python3 src/LLaMIC.py --model_name_or_path_ner $MODEL_PATH_NER --model_name_or_path_nel $MODEL_PATH_NEL --model_name_or_path_review $MODEL_PATH_REVIEW\
+  --input_file $datadir/example.csv --lexicon_path $datadir/lexicon.csv \
+  --run_mode EVAL \
+  --save_annotations yes --save_chunk_size 1 \
+  --n_iterations 1 --window_size 2636 --max_input_tokens 1024 \
+  --output_dir $outdir #--debug
